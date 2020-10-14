ce733875b6feb7410999774deb3d7003141e0915
walgreen <walgreen.melo@gmail.com>
2020-10-14 14:43:30 -0300

    enter

---
+++ aulas caio/Aula 02/exemplo-1/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <text-input color = "green"/>
+  </div>
+</template>
+<script>
+import  TextInput from './components/TextInput'
+
+export default {
+  components: {TextInput}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ aulas caio/Aula 02/exemplo-1/src/components/TextInput.vue
@@ -0,0 +1,36 @@
+<template>
+    <input v-model="text" :style ="style"/>
+</template>
+<script>
+export default {
+    name:"TextInput",
+    props:{
+        color: {
+            type: String,
+            default: "red",
+        },
+        width:{
+            type: Number,
+            default: 1100,
+        }
+    },
+    data(){
+        return{
+            text:'Insira o texto',
+        }
+    },
+    computed:{
+        style(){
+            return{
+                color: this.color,
+                width: (this.text.length * 10 )+ 'px',
+            };
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
